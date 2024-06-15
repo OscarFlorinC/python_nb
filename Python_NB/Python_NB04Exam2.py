ed0c4e787bf86f23143bc084c10c41e775fef9c4
OscarFlocinC <oscar.florinc@icloud.com>
2024-06-14 22:01:39 -0600

    Examen2.py

---
+++ Python_NB/Python_NB04Exam2.py
@@ -0,0 +1,13 @@
+'''Ejercicio 2
+Crear un programa que tenga una variable con la cadena “Separado” y 
+un carácter de coma (,); e inserte el carácter entre cada letra de la cadena. 
+Ej: separar y , debería devolver s,e,p,a,r,a,r
+Pista: Debes utilizar un método de las cadenas llamado “Replace”, recuerda que 
+la posición de los caracteres empieza en 0.'''
+
+
+cadena = "Separado"
+print(cadena)
+
+reemplazar = cadena.replace("" , ",")
+print(reemplazar)
